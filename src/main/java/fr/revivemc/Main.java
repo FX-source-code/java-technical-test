36a3563cd606f188d334ec74408497158abaf118
FX <fxminecraft23@gmail.com>
2026-02-11 20:52:33 +0100

    Ajouté un message de démarrage au plugin

--- src/main/java/fr/revivemc/Main.java
+++ src/main/java/fr/revivemc/Main.java
@@ -5,7 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+        System.out.print("Le plugin a démarré avec succès");
+    }
 
     @Override
     public void onDisable() {}
