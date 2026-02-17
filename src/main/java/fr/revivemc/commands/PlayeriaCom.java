d73cf37ec4239f1ba8ec58b6475b66e268e6319f
FX <fxminecraft23@gmail.com>
2026-02-17 14:24:59 +0100

    fix bug

--- src/main/java/fr/revivemc/commands/PlayeriaCom.java
+++ src/main/java/fr/revivemc/commands/PlayeriaCom.java
@@ -7,12 +7,12 @@ import org.bukkit.command.CommandSender;
 public class PlayeriaCom implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-//        if (args[1].equals("help")) {
-//            commandSender.sendMessage("La liste des commandes disponibles : \n - help");
-//        }
-//        else {
-//            commandSender.sendMessage("§4Mauvaise commande. \n Liste des commandes disponibles avec /playeria : \n - /playeria help");
-//        }
+       if (args[0].equals("help")) {
+           commandSender.sendMessage("La liste des commandes disponibles : \n - help");
+        }
+       else {
+           commandSender.sendMessage("§4Mauvaise commande. \n Liste des commandes disponibles avec /playeria : \n - /playeria help");
+      }
         commandSender.sendMessage("BRUH \n s");
         return false;
     }
