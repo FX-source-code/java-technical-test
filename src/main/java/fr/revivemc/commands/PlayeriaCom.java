55a83aff7f6868039a7fd7db49bbf1d7e521b20c
FX <fxminecraft23@gmail.com>
2026-02-18 14:26:38 +0100

    Amélioré la commande pour qu'il n'y ait plus d'erreur

--- src/main/java/fr/revivemc/commands/PlayeriaCom.java
+++ src/main/java/fr/revivemc/commands/PlayeriaCom.java
@@ -7,13 +7,15 @@ import org.bukkit.command.CommandSender;
 public class PlayeriaCom implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-//        if (args[1].equals("help")) {
-//            commandSender.sendMessage("La liste des commandes disponibles : \n - help");
-//        }
-//        else {
-//            commandSender.sendMessage("§4Mauvaise commande. \n Liste des commandes disponibles avec /playeria : \n - /playeria help");
-//        }
-        commandSender.sendMessage("BRUH \n s");
+        try {
+            if (args[0].equals("help")) {
+                commandSender.sendMessage("La liste des commandes disponibles : \n - help");
+            } else {
+                commandSender.sendMessage("§4Mauvaise commande. \nListe des commandes disponibles avec /playeria : \n - /playeria help");
+            }
+        } catch (Exception e){
+            commandSender.sendMessage("§4Commande mal exécutée. \nFaite /playeria help pour plus d'info");
+        }
         return false;
     }
 }
